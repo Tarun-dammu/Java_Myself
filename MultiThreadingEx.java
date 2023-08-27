8f795ea4834402fd41b69417698130da6dc99229
Tarun-dammu <63595715+Tarun-dammu@users.noreply.github.com>
2023-08-27 12:43:38 +0530

    2nd changes

--- MultiThreadingEx.java
+++ MultiThreadingEx.java
@@ -1,5 +1,8 @@
 public class MultiThreadingEx{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Disco");
+        System.out.println("Coommom");
+        System.out.println("DSp");
     }
 }
